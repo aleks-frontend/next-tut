0794c9e9c2990948b729b31bfb4fd552d3028f10
Aleksandar Gojkovic <aleksfrontend@gmail.com>
2024-08-12 18:00:21 +0200

    style: additional tailwind classes for the featured products

--- sb-03/src/app/products/[productId]/layout.tsx
+++ sb-03/src/app/products/[productId]/layout.tsx
@@ -8,7 +8,11 @@ const ProductDetailLayout: React.FC<Props> = ({ children }) => {
   return (
     <>
       {children}
-      <h1 className="text-center mt-3 underline-offset-1 underline">Featured products</h1>
+      <div className="w-full py-8 bg-gradient-to-r from-blue-900 to-black mt-9">
+        <h1 className="text-white text-3xl font-bold text-center">
+          Featured Products
+        </h1>
+      </div>
     </>
   );
 };
